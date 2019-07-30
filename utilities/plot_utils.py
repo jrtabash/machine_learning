3232f9f1b1535f7f4e8c02df7e099f48ceacf22c
John Tabash <jrtabash@yahoo.com>
2019-07-30 11:40:50 -0500

    Added overlay option to plotPredictVsActual

--- utilities/plot_utils.py
+++ utilities/plot_utils.py
@@ -18,13 +18,25 @@ def plotData(data, xName, yName):
     plt.ylabel(yName)
     plt.show()
 
-def plotPredictVsActual(yPredict, yActual):
+def plotPredictVsActual(yPredict, yActual, overlay=False):
     x = range(0, len(yActual))
-    index = 1
-    for yDataColorAndName in [(yPredict, "c", "Prediction"), (yActual, "r", "Actual")]:
-        plt.subplot(2, 1, index)
-        plt.plot(x, yDataColorAndName[0], yDataColorAndName[1])
-        plt.ylabel(yDataColorAndName[2])
+    yDataColorAndNameList = [(yPredict, "c", "Prediction"), (yActual, "r", "Actual")]
+    titleText = "Prediction vs. Actual"
+
+    if overlay:
+        for yDataColorAndName in yDataColorAndNameList:
+            plt.plot(x, yDataColorAndName[0], yDataColorAndName[1], label=yDataColorAndName[2])
+        plt.title(titleText)
         plt.grid(axis="both")
-        index += 1
+        plt.legend()
+    else:
+        index = 1
+        for yDataColorAndName in yDataColorAndNameList:
+            plt.subplot(2, 1, index)
+            plt.plot(x, yDataColorAndName[0], yDataColorAndName[1])
+            plt.ylabel(yDataColorAndName[2])
+            if index == 1:
+                plt.title(titleText)
+            plt.grid(axis="both")
+            index += 1
     plt.show()
