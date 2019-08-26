ec0f275087ed35b7efb6a030e16f5d8f2fa38396
John Tabash <jrtabash@yahoo.com>
2019-08-25 19:01:03 -0500

    Reworked getMetroTrafficData implementation.

--- metro_traffic/metro_traffic.py
+++ metro_traffic/metro_traffic.py
@@ -5,21 +5,30 @@ import data_utils
 import misc_utils
 import datetime_utils
 
-def getMetroTrafficData(path="~/Data/MetroInterstateTrafficVolume/", reindex=False, temp=None):
+def readMetroTrafficCSV(path="~/Data/MetroInterstateTrafficVolume/"):
     mt = pd.read_csv(path + "Metro_Interstate_Traffic_Volume.csv")
 
     mt.date_time = np.vectorize(pd.Timestamp)(mt.date_time)
-    if reindex:
-        mt.index = mt.date_time
-        mt = mt.drop(columns=['date_time'])
-
-    if temp == 'C':
-        mt.temp = np.vectorize(misc_utils.convertK2C)(mt.temp)
-    elif temp == 'F':
-        mt.temp = np.vectorize(misc_utils.convertK2F)(mt.temp)
 
     mt.holiday = mt.holiday.astype('category')
     mt.weather_main = mt.weather_main.astype('category')
     mt.weather_description = mt.weather_description.astype('category')
 
+    return mt
+
+def updateMetroTrafficData(data, reindex=False, temp=None):
+    if reindex:
+        data.index = data.date_time
+        data = data.drop(columns=['date_time'])
+
+    if temp == 'C':
+        data.temp = np.vectorize(misc_utils.convertK2C)(data.temp)
+    elif temp == 'F':
+        data.temp = np.vectorize(misc_utils.convertK2F)(data.temp)
+
+    return data
+
+def getMetroTrafficData(dateTimeIndex=False, temp=None):
+    mt = readMetroTrafficCSV()
+    mt = updateMetroTrafficData(mt, reindex=dateTimeIndex, temp=temp)
     return mt
