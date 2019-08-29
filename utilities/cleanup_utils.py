cca8bb1d322ae3d01e866b07072f4e231386d07b
John Tabash <jrtabash@yahoo.com>
2019-08-28 21:23:56 -0500

    Added GapsProcessor class description.

--- utilities/cleanup_utils.py
+++ utilities/cleanup_utils.py
@@ -5,6 +5,48 @@ import data_utils
 #
 # Gaps Processor
 #
+# This class is for filling missing data in which full observation
+# rows are missing and nothing is marked as missing with NA.
+#
+# An example of such data might look something like this:
+#
+#             date_time    temp  rain_1h weather_main  traffic_volume
+# 0 2012-10-03 03:00:00  281.09      0.0        Clear             367
+# 1 2012-10-03 04:00:00  279.53      0.0        Clear             814
+# 2 2012-10-03 05:00:00  278.62      0.0        Clear            2718
+# 3 2012-10-03 06:00:00  278.23      0.0        Clear            5673
+# 4 2012-10-03 09:00:00  282.48      0.0        Clear            6471
+# 5 2012-10-03 10:00:00  283.12      0.0        Clear            5511
+#
+# Note at a first glance, the DataFrame looks ok, but two measurements
+# or rows are missing, 07:00:00 and 08:00:00.
+#
+# Given the follwing:
+#   columnIndex: 0 (column index of date_time column)
+#   findGapsFtn: returning list of index pairs
+#                e.g. [(3, 4)]
+#   gapRangeFtn: returning a range of Timestamps between given date_time values
+#                e.g. For date_time at index 3 and date_time at index 4 return
+#                     [Timestamp('2012-10-03 06:00:00'),
+#                      Timestamp('2012-10-03 07:00:00'),
+#                      Timestamp('2012-10-03 08:00:00')]
+#
+# Then for each of the following actions, the following rows will be added:
+# action: CarryForward
+# rows:
+# 4 2012-10-03 07:00:00  278.23      0.0        Clear            5673
+# 5 2012-10-03 08:00:00  278.23      0.0        Clear            5673
+#
+# action: CarryBackward
+# rows:
+# 4 2012-10-03 07:00:00  282.48      0.0        Clear            6471
+# 5 2012-10-03 08:00:00  282.48      0.0        Clear            6471
+#
+# action: FillAverage
+# rows:
+# 4 2012-10-03 07:00:00  280.35      0.0        Clear            6072
+# 5 2012-10-03 08:00:00  280.35      0.0        Clear            6072
+#
 class GapsProcessor:
     class Action:
         CarryForward  = 0
