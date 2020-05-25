ad3ca7dfa1509e30c0944de1ee25e1ca8b03e7c1
John Tabash <jrtabash@yahoo.com>
2020-05-25 18:39:35 -0500

    Continuing work on the car parking experiment.
    * Updated addFeatures function
    * Updated getData function
    * Added makePipeline function
    * Added plotKMeansInertia function

--- car_parking/car_parking.py
+++ car_parking/car_parking.py
@@ -1,5 +1,10 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+from sklearn.decomposition import PCA
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
 
 DefaultCSV = '~/Data/ParkingBirmingham/parking_birmingham.csv'
 
@@ -19,25 +24,64 @@ def reindexData(data):
     data.drop(columns=['update_time'], inplace=True)
     return data
 
-def addFeatures(data):
-    maxOccupancy = data[['system_code', 'occupancy']].groupby(by=['system_code']).max().reset_index(drop=False)
-    maxOccupancy.index = maxOccupancy.system_code
-    maxOccupancy.drop(columns=['system_code'], inplace=True)
+def addFeatures(data, rateFeatures=True, timeFeatures=False):
+    #  rate := Occupancy rate based on capacity
+    # hrate := Occupancy rate based on historic max occupancy
+    if rateFeatures:
+        maxOccupancy = data[['system_code', 'occupancy']].groupby(by=['system_code']).max().reset_index(drop=False)
+        maxOccupancy.index = maxOccupancy.system_code
+        maxOccupancy.drop(columns=['system_code'], inplace=True)
 
-    getMaxOccupancy = np.vectorize(lambda c: maxOccupancy[c:c].values[0][0])
+        getMaxOccupancy = np.vectorize(lambda c: maxOccupancy[c:c].values[0][0])
 
-    data['rate'] = data.occupancy / np.float64(data.capacity)
-    data['hrate'] = data.occupancy / getMaxOccupancy(data.system_code)
-    data['weekday'] = np.vectorize(lambda x: x.isoweekday())(data.reset_index(drop=False).update_time.dt.date)
+        data['rate'] = data.occupancy / np.float64(data.capacity)
+        data['hrate'] = data.occupancy / getMaxOccupancy(data.system_code)
+
+    #  mnth := Month Jan=1, Feb=2, ...
+    #  wkdy := Day of week 1=Monday, 2=Tuesday, ...
+    #   ssm := Seconds since midnight
+    if timeFeatures:
+        dts = data.update_time.dt
+
+        data['mnth'] = np.vectorize(lambda x: x.month)(dts.date)
+        data['wkdy'] = np.vectorize(lambda x: x.isoweekday())(dts.date)
+        data['ssm'] = np.vectorize(lambda t: (t.hour * 3600) + (t.minute * 60) + (t.second))(dts.time.values)
 
     return data
 
-def getData(csvFile=DefaultCSV, reindex=True, cleanup=True, features=True):
+def getData(csvFile=DefaultCSV, reindex=True, cleanup=True, rateFeatures=True, timeFeatures=False, columnsToDrop=None):
     data = readData(csvFile)
     if cleanup:
         data = cleanupData(data)
+    if rateFeatures or timeFeatures:
+        data = addFeatures(data, rateFeatures, timeFeatures)
     if reindex:
         data = reindexData(data)
-    if features:
-        data = addFeatures(data)
+    if columnsToDrop:
+        data.drop(columns=columnsToDrop, inplace=True)
     return data
+
+def makePipeline(data, scaler=None, pca=None, clusters=None):
+    pipeline=None
+    steps = []
+    if scaler:
+        steps.append(('scaler', StandardScaler() if scaler == 'std' else MinMaxScaler()))
+    if pca:
+        steps.append(('pca', PCA(n_components=pca)))
+    if clusters:
+        steps.append(('kmeans', KMeans(n_clusters=clusters)))
+
+    if len(steps) > 0:
+        pipeline = Pipeline(steps).fit(data)
+
+    return pipeline
+
+def plotKMeansInertia(data, ns=range(1, 10)):
+    inertias = [KMeans(n_clusters=n).fit(data).inertia_ for n in ns]
+
+    plt.plot(ns, inertias, '-o')
+    plt.title('KMeans Inertia')
+    plt.xlabel('clusters')
+    plt.ylabel('Inertia')
+    plt.grid(True)
+    plt.show()
